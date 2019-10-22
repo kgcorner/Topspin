ea03ec7ba4bbb4f2398188dbd8e26f33dc9b84a0
Kumar <kumargauravgupta3@gmail.com>
2019-10-22 08:16:43 +0530

    Adds unit test for FacebookOAuthService

---
+++ auth-service/src/test/java/com/kgcorner/topspin/service/facebook/FacebookOAuthServiceTest.java
@@ -0,0 +1,94 @@
+package com.kgcorner.topspin.service.facebook;
+
+import com.kgcorner.web.HttpUtil;
+import kong.unirest.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+/*
+Description : <Write class Description>
+Author: kumar
+Created on : 22/10/19
+*/
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(HttpUtil.class)
+public class FacebookOAuthServiceTest {
+    private FacebookOAuthService facebookOAuthService;
+    private FacebookConfigProvider mockedFacebookConfigProvider;
+
+    @Before
+    public void setUp() throws Exception {
+        facebookOAuthService = new FacebookOAuthService();
+        mockedFacebookConfigProvider = PowerMockito.mock(FacebookConfigProvider.class);
+        Whitebox.setInternalState(facebookOAuthService, "facebookConfigProvider", mockedFacebookConfigProvider);
+    }
+
+    @Test
+    public void getAccessToken() {
+        when(mockedFacebookConfigProvider.getAccessTokenExchangeUrl("redirect_uri", "auth_code"))
+            .thenReturn("exchange_url");
+        String accessToken = "access_token";
+        HttpResponse mockedResponse = mock(HttpResponse.class);
+        mockStatic(HttpUtil.class);
+        when(HttpUtil.doGet("exchange_url", null, null))
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getStatus()).thenReturn(HttpStatus.SC_OK);
+        when(mockedResponse.getBody()).thenReturn(prepareFbResponseForAccessToken(accessToken));
+        String response = facebookOAuthService.getAccessToken("auth_code", "redirect_uri");
+        Assert.assertNotNull("Returned access token is null", response);
+        Assert.assertEquals("access token is not matching", accessToken, response);
+    }
+
+    @Test
+    public void getAccessTokenForInvalidAuthCode() {
+        when(mockedFacebookConfigProvider.getAccessTokenExchangeUrl("redirect_uri", "auth_code"))
+            .thenReturn("exchange_url");
+        String accessToken = "access_token";
+        HttpResponse mockedResponse = mock(HttpResponse.class);
+        mockStatic(HttpUtil.class);
+        when(HttpUtil.doGet("exchange_url", null, null))
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getStatus()).thenReturn(HttpStatus.SC_BAD_REQUEST);
+        when(mockedResponse.getBody()).thenReturn(prepareFbResponseForAccessToken(accessToken));
+        String response = facebookOAuthService.getAccessToken("auth_code", "redirect_uri");
+        Assert.assertNull("Returned access token is not null", response);
+    }
+
+    @Test
+    public void validateAccessToken() {
+        String accessToken = "access_token";
+        when(mockedFacebookConfigProvider.getAccessTokenValidationUrl(accessToken))
+            .thenReturn("validation_url");
+        HttpResponse mockedResponse = mock(HttpResponse.class);
+        mockStatic(HttpUtil.class);
+        when(HttpUtil.doGet("validation_url", null, null))
+            .thenReturn(mockedResponse);
+        when(mockedResponse.isSuccess()).thenReturn(true);
+        boolean response = facebookOAuthService.validateAccessToken(accessToken);
+        Assert.assertTrue("access token validation failed", response);
+    }
+
+    @Test
+    public void getOAuthServiceName() {
+        assertEquals("OAuth service name is not matching","facebook",
+            facebookOAuthService.getOAuthServiceName());
+    }
+
+    private String prepareFbResponseForAccessToken(String accessToken) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("access_token", accessToken);
+        return jsonObject.toString();
+    }
+}

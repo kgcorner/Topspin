9c114576cb1a3b75a1f7a91a02140592554bff45
Kumar <kumargauravgupta3@gmail.com>
2019-11-07 23:18:02 +0530

    Include negative test cases for integration test

--- integration-tests/authservice-tests/src/test/java/com/kgcorner/topspin/stepdefs/NativeLoginTests.java
+++ integration-tests/authservice-tests/src/test/java/com/kgcorner/topspin/stepdefs/NativeLoginTests.java
@@ -12,6 +12,7 @@ import com.kgcorner.topspin.stepdefs.model.Token;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import gherkin.deps.com.google.gson.Gson;
+import org.apache.http.HttpStatus;
 import org.testng.Assert;
 
 import java.util.Base64;
@@ -21,17 +22,20 @@ import java.util.Map;
 public class NativeLoginTests {
     private URLs urls = URLs.getInstance();
     private Response RESPONSE_FOR_VALID_NATIVE_LOGIN = null;
-
+    private Response RESPONSE_FOR_BEARER_TOKEN_LOGIN = null;
+    private Token token;
     @Then("^Service should revert with a token object and status '(\\d+)'$")
     public void serviceShouldRevertWithATokenObjectAndStatus(int status) {
         Assert.assertEquals(RESPONSE_FOR_VALID_NATIVE_LOGIN.getStatus(), status,
             "Status for RESPONSE_FOR_VALID_NATIVE_LOGIN is not matching");
-        Gson gson = new Gson();
-        final Token token = gson.fromJson(RESPONSE_FOR_VALID_NATIVE_LOGIN.getData(), Token.class);
-        Assert.assertNotNull(token, "received token is null");
-        Assert.assertNotNull(token.getAccessToken(), "Received access token is null");
-        Assert.assertNotNull(token.getExpiresInSeconds(), "Received expiration time is null");
-        Assert.assertNotNull(token.getRefreshToken(), "Received refresh token is null");
+        if(status == HttpStatus.SC_OK) {
+            Gson gson = new Gson();
+            final Token token = gson.fromJson(RESPONSE_FOR_VALID_NATIVE_LOGIN.getData(), Token.class);
+            Assert.assertNotNull(token, "received token is null");
+            Assert.assertNotNull(token.getAccessToken(), "Received access token is null");
+            Assert.assertNotNull(token.getExpiresInSeconds(), "Received expiration time is null");
+            Assert.assertNotNull(token.getRefreshToken(), "Received refresh token is null");
+        }
     }
 
     @Given("^User exists with user name \"([^\"]*)\" and password \"([^\"]*)\"$")
@@ -44,4 +48,42 @@ public class NativeLoginTests {
         RESPONSE_FOR_VALID_NATIVE_LOGIN = HttpUtil.doGet(tokenUrl, headers);
     }
 
+    @Then("^User logs in in the system to obtain an access token$")
+    public void userLogsInInTheSystemToObtainAnAccessToken() {
+        Assert.assertNotNull(RESPONSE_FOR_VALID_NATIVE_LOGIN, "Login responds with null");
+        token = new Gson().fromJson(RESPONSE_FOR_VALID_NATIVE_LOGIN.getData(), Token.class);
+        Assert.assertNotNull(token, "Login api responds with invalid token response");
+        Assert.assertNotNull(token.getRefreshToken(), "No refresh token available in token");
+        Assert.assertNotNull(token.getAccessToken(), "No access token available in token");
+        Assert.assertTrue(token.getExpiresInSeconds() > 1, "Api responds with invalid expiration value");
+
+    }
+
+    @Then("^User tries to validate that given access token$")
+    public void userTriesToValidateThatGivenAccessToken() {
+        String tokenUrl = urls.getTokenUrl();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + token.getAccessToken());
+        RESPONSE_FOR_BEARER_TOKEN_LOGIN = HttpUtil.doGet(tokenUrl, headers);
+    }
+
+    @Then("^server should revert the token with status '(\\d+)'$")
+    public void serverShouldRevertTheTokenWithStatus(int status) {
+        Assert.assertEquals(RESPONSE_FOR_BEARER_TOKEN_LOGIN.getStatus(), status, "Response status is not matching");
+        if(status == HttpStatus.SC_OK) {
+            Assert.assertNotNull(RESPONSE_FOR_BEARER_TOKEN_LOGIN, "Login responds with null");
+            token = new Gson().fromJson(RESPONSE_FOR_BEARER_TOKEN_LOGIN.getData(), Token.class);
+            Assert.assertNotNull(token, "Login api responds with invalid token response");
+            Assert.assertNotNull(token.getAccessToken(), "No access token available in token");
+        }
+
+    }
+
+    @Given("^User tries to validate invalid bearer token$")
+    public void userTriesToValidateInvalidBearerToken() {
+        String tokenUrl = urls.getTokenUrl();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer invalid_bearer_token");
+        RESPONSE_FOR_BEARER_TOKEN_LOGIN = HttpUtil.doGet(tokenUrl, headers);
+    }
 }
--- integration-tests/authservice-tests/src/test/resources/features/native-login.feature
+++ integration-tests/authservice-tests/src/test/resources/features/native-login.feature
@@ -1,5 +1,21 @@
 @NativeLogin
 Feature: Native Login
-  Scenario: User tries to login with valid credential
+
+  Scenario Outline: User tries to login with valid credential
+    Given User exists with user name "<user>" and password "<password>"
+    Then Service should revert with a token object and status '<status>'
+    Examples:
+      | user    | status | password   |
+      | user    | 200    |  password  |
+      | user1   | 403    |  password1 |
+
+  Scenario: User tries to get authenticated using bearer token
     Given User exists with user name "user" and password "password"
-    Then Service should revert with a token object and status '200'
+    Then User logs in in the system to obtain an access token
+    Then User tries to validate that given access token
+    Then server should revert the token with status '200'
+
+  Scenario: User tries to get authenticated using invalid bearer token
+    Given User tries to validate invalid bearer token
+    Then server should revert the token with status '403'
+

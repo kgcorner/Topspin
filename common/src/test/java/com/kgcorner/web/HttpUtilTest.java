05cd7a967e94849e1648075625fb0e5f4aab32f7
Kumar <kumargauravgupta3@gmail.com>
2019-11-19 09:33:29 +0530

    cover more lines

--- common/src/test/java/com/kgcorner/web/HttpUtilTest.java
+++ common/src/test/java/com/kgcorner/web/HttpUtilTest.java
@@ -1,9 +1,6 @@
 package com.kgcorner.web;
 
-import kong.unirest.GetRequest;
-import kong.unirest.HttpRequestWithBody;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
+import kong.unirest.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -45,6 +42,19 @@ public class HttpUtilTest {
     }
 
     @Test
+    public void doGetWithoutQueryAndHeader() {
+        String url = "url";
+        String data = "data";
+        HttpResponse<String> stringHttpResponse = mock(HttpResponse.class);
+        GetRequest request = mock(GetRequest.class);
+        mockStatic(Unirest.class);
+        when(Unirest.get(url)).thenReturn(request);
+        when(request.asString()).thenReturn(stringHttpResponse);
+        HttpResponse httpResponse = HttpUtil.doGet(url, null, null);
+        assertNotNull(httpResponse);
+    }
+
+    @Test
     public void doPost() {
         String url = "url";
         String data = "data";
@@ -54,6 +64,7 @@ public class HttpUtilTest {
         when(Unirest.post(url)).thenReturn(request);
         when(request.queryString("a","b")).thenReturn(request);
         when(request.header("a","b")).thenReturn(request);
+
         when(request.asString()).thenReturn(stringHttpResponse);
         Map<String, String> queries = new HashMap<>();
         queries.put("a", "b");
@@ -62,6 +73,40 @@ public class HttpUtilTest {
         Map<String, String> body = new HashMap<>();
         body.put("a", "b");
         HttpResponse httpResponse = HttpUtil.doPost(url, queries, headers, body);
+        assertNotNull(httpResponse);
+    }
+
+    @Test
+    public void doPostWithJustBody() {
+        String url = "url";
+        String data = "data";
+        HttpResponse<String> stringHttpResponse = mock(HttpResponse.class);
+        HttpRequestWithBody request = mock(HttpRequestWithBody.class);
+        MultipartBody multipartBody = mock(MultipartBody.class);
+        mockStatic(Unirest.class);
+        when(Unirest.post(url)).thenReturn(request);
+        when(multipartBody.asString()).thenReturn(stringHttpResponse);
+        when(request.field("a","b")).thenReturn(multipartBody);
+        when(multipartBody.field("c","d")).thenReturn(multipartBody);
+        Map<String, String> body = new HashMap<>();
+        body.put("a", "b");
+        body.put("c", "d");
+        HttpResponse httpResponse = HttpUtil.doPost(url, null, null, body);
+        assertNotNull(httpResponse);
+    }
+
+    @Test
+    public void doPostWithoutBody() {
+        String url = "url";
+        String data = "data";
+        HttpResponse<String> stringHttpResponse = mock(HttpResponse.class);
+        HttpRequestWithBody request = mock(HttpRequestWithBody.class);
+        MultipartBody multipartBody = mock(MultipartBody.class);
+        mockStatic(Unirest.class);
+        when(Unirest.post(url)).thenReturn(request);
+        when(request.asString()).thenReturn(stringHttpResponse);
+        when(request.field("a","b")).thenReturn(multipartBody);
+        HttpResponse httpResponse = HttpUtil.doPost(url, null, null, null);
         assertNotNull(httpResponse);
     }
 }

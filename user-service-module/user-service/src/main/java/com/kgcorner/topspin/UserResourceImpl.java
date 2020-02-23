3a3a1a3108836c5db7556e959b3b46a6fcf4587c
Kumar <kumargauravgupta3@gmail.com>
2020-02-23 20:10:25 +0530

    Adds api for health check and annotate create user with Create response status

--- user-service-module/user-service/src/main/java/com/kgcorner/topspin/UserResourceImpl.java
+++ user-service-module/user-service/src/main/java/com/kgcorner/topspin/UserResourceImpl.java
@@ -6,6 +6,7 @@ import com.kgcorner.topspin.services.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,32 +16,23 @@ import java.util.List;
  * Author: kumar
  * Created on : 02/12/19
  */
-
+@RestController
 public class UserResourceImpl extends ExceptionHandler implements UserResource {
 
     @Autowired
     private UserService userService;
 
-    @ApiOperation("Returns user with given id")
-    @GetMapping("/users/{user-id")
-    @Override
-    public User getUser( @ApiParam(value = "Id of the user", required = true)
-         @PathVariable("user-id") String userId) {
-        return userService.getUser(userId);
+
+    @GetMapping("/health")
+    public String getHealth() {
+        return "Ok";
     }
 
-    @ApiOperation("Returns list of users page wise")
-    @GetMapping("/users?page={page-num}")
-    @Override
-    public List<User> getAllUsers(@ApiParam(value = "Page number", required = true)
-                                      @RequestParam("page-num") int page,
-                                  @ApiParam(value = "users per page, default 30", required = false, defaultValue = "30")
-                                  @RequestParam("page-num") int maxPerPage) {
-        return userService.getAllUsers(page, maxPerPage);
-    }
+
 
     @ApiOperation("Creates user")
     @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@ApiParam("name of the user") @RequestParam("name") String name,
                            @ApiParam("username of the user") @RequestParam("username") String userName,
                            @ApiParam("email of the user") @RequestParam("email") String email,
@@ -60,5 +52,23 @@ public class UserResourceImpl extends ExceptionHandler implements UserResource {
         @ApiParam("any other info") @RequestParam("other") String other
     ) {
         return userService.updateUser(id, name, email, contact, other);
+    }
+
+    @ApiOperation("Returns user with given id")
+    @GetMapping("/users/{user-id}")
+    @Override
+    public User getUser( @ApiParam(value = "Id of the user", required = true)
+                         @PathVariable("user-id") String userId) {
+        return userService.getUser(userId);
+    }
+
+    @ApiOperation("Returns list of users page wise")
+    @GetMapping("/users")
+    @Override
+    public List<User> getAllUsers(@ApiParam(value = "Page number", required = true)
+                                  @RequestParam("page") int page,
+                                  @ApiParam(value = "users per page, default 30", required = false, defaultValue = "30")
+                                  @RequestParam("items") int maxPerPage) {
+        return userService.getAllUsers(page, maxPerPage);
     }
 }

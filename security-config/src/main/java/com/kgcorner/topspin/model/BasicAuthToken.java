2180389d88e4808d990bcfc2ab7db72956ab09ce
Kumar <kumargauravgupta3@gmail.com>
2020-02-23 21:29:34 +0530

    Removes custom implementation of implies

--- security-config/src/main/java/com/kgcorner/topspin/model/BasicAuthToken.java
+++ security-config/src/main/java/com/kgcorner/topspin/model/BasicAuthToken.java
@@ -9,7 +9,6 @@ Created on : 3/2/19
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.security.auth.Subject;
 import java.util.Collection;
 
 public class BasicAuthToken extends UsernamePasswordAuthenticationToken {
@@ -20,10 +19,5 @@ public class BasicAuthToken extends UsernamePasswordAuthenticationToken {
 
     public BasicAuthToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
         super(principal, credentials, authorities);
-    }
-
-    @Override
-    public boolean implies(Subject subject) {
-        return false;
     }
 }

2ff01897b539e419c8b21b99896f1a23eb356bb4
Kumar <kumargauravgupta3@gmail.com>
2020-02-29 12:57:30 +0530

    fixes issue with name of "username"

--- auth-service-modules/auth-db-common/src/main/java/com/kgcorner/topspin/model/Login.java
+++ auth-service-modules/auth-db-common/src/main/java/com/kgcorner/topspin/model/Login.java
@@ -10,7 +10,7 @@ Created on : 26/08/19
 import java.io.Serializable;
 
 public interface Login extends Serializable, Requester {
-    String USER_NAME_COLUMN = "userName";
+    String USER_NAME_COLUMN = "username";
 
     String getId();
 

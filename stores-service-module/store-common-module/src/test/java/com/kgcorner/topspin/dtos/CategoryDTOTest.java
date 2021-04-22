f498f9c2ecad527dd32015459ccc75be390aea91
Kumar <kumargauravgupta3@gmail.com>
2021-04-22 09:04:57 +0530

    Adds more test cases

--- stores-service-module/store-common-module/src/test/java/com/kgcorner/topspin/dtos/CategoryDTOTest.java
+++ stores-service-module/store-common-module/src/test/java/com/kgcorner/topspin/dtos/CategoryDTOTest.java
@@ -1,10 +1,13 @@
 package com.kgcorner.topspin.dtos;
 
 import com.kgcorner.topspin.model.AbstractCategory;
+import com.kgcorner.topspin.model.Category;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.powermock.api.mockito.PowerMockito;
+
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -16,10 +19,10 @@ import org.powermock.api.mockito.PowerMockito;
 public class CategoryDTOTest {
 
     private CategoryDTO categoryDTO;
-    private AbstractCategory category;
+    private DemoCategory category;
     @Before
     public void setUp() {
-        category = PowerMockito.mock(AbstractCategory.class);
+        category = new DemoCategory();
         categoryDTO = new CategoryDTO(category);
     }
 
@@ -30,5 +33,85 @@ public class CategoryDTOTest {
             categoryDTO.addChildren(new CategoryDTO(category));
         }
         Assert.assertEquals(maxCount, categoryDTO.getChildren().size());
+    }
+
+    @Test
+    public void setCategoryId() {
+        String categoryId = "categoryId";
+        category.setCategoryId(categoryId);
+        Assert.assertEquals(categoryId, categoryDTO.getCategoryId());
+    }
+
+    @Test
+    public void setName() {
+        String name = "name";
+        category.setName(name);
+        Assert.assertEquals(name, categoryDTO.getName());
+    }
+
+    @Test
+    public void setDescription() {
+        String description = "description";
+        category.setDescription(description);
+        Assert.assertEquals(description, categoryDTO.getDescription());
+    }
+
+    @Test
+    public void setLongDescription() {
+        String longDescription = "longDescription";
+        category.setLongDescription(longDescription);
+        Assert.assertEquals(longDescription, categoryDTO.getLongDescription());
+    }
+
+    @Test
+    public void setBannerImage() {
+        String bannerImage = "bannerImage";
+        category.setBannerImage(bannerImage);
+        Assert.assertEquals(bannerImage, categoryDTO.getBannerImage());
+    }
+
+    @Test
+    public void setThumbnailImage() {
+        String thumbnailImage = "thumbnailImage";
+        category.setThumbnailImage(thumbnailImage);
+        Assert.assertEquals(thumbnailImage, categoryDTO.getThumbNailImage());
+    }
+
+    @Test
+    public void setLargeImage() {
+        String largerImage = "largerImage";
+        category.setLargeImage(largerImage);
+        Assert.assertEquals(largerImage, categoryDTO.getLargeImage());
+    }
+
+    @Test
+    public void setParent() {
+        Category parent = new DemoCategory();
+        category.setParent(parent);
+        Assert.assertEquals(parent, categoryDTO.getParent());
+    }
+
+    @Test
+    public void setTagline() {
+        String tagline = "tagline";
+        category.setTagline(tagline);
+        Assert.assertEquals(tagline, categoryDTO.getTagLine());
+    }
+
+    class DemoCategory extends AbstractCategory {
+        private String cateId;
+
+        public void setCategoryId(String cateId) {
+            this.cateId = cateId;
+        }
+        @Override
+        public String getCategoryId() {
+            return cateId;
+        }
+
+        @Override
+        public List<? extends Category> getChildren() {
+            return Collections.emptyList();
+        }
     }
 }
--- stores-service-module/store-common-module/src/test/java/com/kgcorner/topspin/dtos/StoreDTOTest.java
+++ stores-service-module/store-common-module/src/test/java/com/kgcorner/topspin/dtos/StoreDTOTest.java
@@ -1,6 +1,7 @@
 package com.kgcorner.topspin.dtos;
 
 import com.kgcorner.topspin.model.AbstractStore;
+import com.kgcorner.topspin.model.Category;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,11 +18,13 @@ import java.util.List;
 
 public class StoreDTOTest {
 
-    private StoreDTO storeDTO;
-    private AbstractStore store;
+    private StoreDTO storeDto;
+    private DemoStore store;
+
     @Before
     public void setUp() {
-        storeDTO = new StoreDTO(store);
+        store = new DemoStore();
+        storeDto = new StoreDTO(store);
     }
 
 
@@ -32,9 +35,176 @@ public class StoreDTOTest {
         for (int i = 0; i < 10; i++) {
             CategoryDTO categoryDTO = new CategoryDTO(null);
             categoryDTOS.add(categoryDTO);
-            storeDTO.addCategories(categoryDTO);
+            storeDto.addCategories(categoryDTO);
         }
 
-        Assert.assertEquals(categoryDTOS, storeDTO.getCategories());
+        Assert.assertEquals(categoryDTOS, storeDto.getCategories());
+    }
+
+    @Test
+    public void setStoreId() {
+        String storeId = "storeID";
+        store.setStoreId(storeId);
+        Assert.assertEquals(storeId, storeDto.getStoreId());
+    }
+
+    @Test
+    public void setName() {
+        String name = "Amazon";
+        store.setName(name);
+        Assert.assertEquals(name, storeDto.getName());
+    }
+
+    @Test
+    public void setLink() {
+        String link = "link";
+        store.setLink(link);
+        Assert.assertEquals(link, storeDto.getLink());
+    }
+
+    @Test
+    public void setLogo() {
+        String logo = "logo";
+        store.setLogo(logo);
+        Assert.assertEquals(logo, storeDto.getLogo());
+    }
+
+    @Test
+    public void setOpenOut() {
+        boolean openOut = true;
+        store.setOpenOut(openOut);
+        Assert.assertTrue(storeDto.isOpenOut());
+    }
+
+    @Test
+    public void setAffiliated() {
+        boolean affiliated =true;
+        store.setAffiliated(affiliated);
+        Assert.assertTrue(storeDto.isAffiliated());
+    }
+
+    @Test
+    public void setPlaceHolder() {
+        String placeHolder = "placeHolder";
+        store.setPlaceHolder(placeHolder);
+        Assert.assertEquals(placeHolder, storeDto.getPlaceHolder());
+    }
+
+    @Test
+    public void setSurferPlaceHolder() {
+        String SurferPlaceHolder = "SurferPlaceHolder";
+        store.setSurferPlaceHolder(SurferPlaceHolder);
+        Assert.assertEquals(SurferPlaceHolder, storeDto.getSurferPlaceHolder());
+    }
+
+    @Test
+    public void setSearchUrl() {
+        String searchUrl = "searchUrl";
+        store.setSearchUrl(searchUrl);
+        Assert.assertEquals(searchUrl, storeDto.getSearchUrl());
+    }
+
+    @Test
+    public void setFixedUrls() {
+        String fixedUrl = "fixedUrl";
+        store.setFixedUrls(fixedUrl);
+        Assert.assertEquals(fixedUrl, storeDto.getFixedUrls());
+    }
+
+    @Test
+    public void setPidRegex() {
+        String regex = "regex";
+        store.setPidRegex(regex);
+        Assert.assertEquals(regex, storeDto.getPidRegex());
+    }
+
+    @Test
+    public void setGender() {
+        String gender = "Male";
+        store.setGender(gender);
+        Assert.assertEquals(gender, storeDto.getGender());
+    }
+
+    @Test
+    public void setAffiliateId() {
+        String affiliateId = "affiliateId";
+        store.setAffiliateId(affiliateId);
+        Assert.assertEquals(affiliateId, storeDto.getAffiliateId());
+    }
+
+    @Test
+    public void setMaxCashback() {
+        String cashback = "50%";
+        store.setMaxCashback(cashback);
+        Assert.assertEquals(cashback, storeDto.getMaxCashback());
+    }
+
+    @Test
+    public void setDescription() {
+        String description = "description";
+        store.setDescription(description);
+        Assert.assertEquals(description, storeDto.getDescription());
+    }
+
+    @Test
+    public void setLongDescription() {
+        String longDescription = "longDescription";
+        store.setLongDescription(longDescription);
+        Assert.assertEquals(longDescription, storeDto.getLongDescription());
+    }
+
+    @Test
+    public void setBannerImage() {
+        String bannerImage = "bannerImage";
+        store.setBannerImage(bannerImage);
+        Assert.assertEquals(bannerImage, storeDto.getBannerImage());
+    }
+
+    @Test
+    public void setThumbnailImage() {
+        String thumbNailImage = "thumbNailImage";
+        store.setThumbnailImage(thumbNailImage);
+        store.setThumbnailImage(thumbNailImage);
+        Assert.assertEquals(thumbNailImage, storeDto.getThumbnailImage());
+    }
+
+    @Test
+    public void setLargeImage() {
+        String largeImage = "largeImage";
+        store.setLargeImage(largeImage);
+        Assert.assertEquals(largeImage, storeDto.getLargeImage());
+    }
+
+    @Test
+    public void setTagLine() {
+        String tagLine = "tagLine";
+        store.setTagLine(tagLine);
+        Assert.assertEquals(tagLine, storeDto.getTagLine());
+    }
+
+    @Test
+    public void setActive() {
+        boolean active = true;
+        store.setActive(active);
+        Assert.assertTrue(storeDto.isActive());
+    }
+
+    class DemoStore extends AbstractStore {
+        private String storeId;
+        private List<Category> categories = new ArrayList<>();
+
+        public void setStoreId(String id) {
+            this.storeId = id;
+        }
+
+        @Override
+        public String getStoreId() {
+            return storeId;
+        }
+
+        @Override
+        public List<? extends Category> getCategories() {
+            return categories;
+        }
     }
 }

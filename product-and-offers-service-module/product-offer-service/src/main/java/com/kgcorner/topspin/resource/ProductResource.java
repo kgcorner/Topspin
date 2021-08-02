0914f1b18956bf6e68945f3cd11a7223a1e2b6d3
Kumar <kumargauravgupta3@gmail.com>
2021-08-03 01:34:55 +0530

    test

--- product-and-offers-service-module/product-offer-service/src/main/java/com/kgcorner/topspin/resource/ProductResource.java
+++ product-and-offers-service-module/product-offer-service/src/main/java/com/kgcorner/topspin/resource/ProductResource.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +24,7 @@ public class ProductResource {
     private ProductService productService;
 
     @ApiOperation("Creates a product")
-    @PostMapping("/products")
+    @PostMapping(value = "/products", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<ProductDTO> createProduct(
         @ApiParam("Title of the product")
         @RequestParam("title") String title,
@@ -52,10 +53,7 @@ public class ProductResource {
     ) {
         ProductDTO product = productService.createProduct(title, description, price, discountedPrice, currency, smallImageUrl,
             mediumImageUrl, largeImageUrl, categoryId, storeId, brand, url);
-        Link selfLink = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(ProductResource.class)
-            .getProduct(product.getProductId())).withSelfRel();
-        product.add(selfLink);
-        return ResponseEntity.ok(product);
+        return getProductDTOResponseEntity(product);
     }
 
     @ApiOperation("Get product by id")
@@ -65,6 +63,48 @@ public class ProductResource {
         @PathVariable("id")
         String productId) {
         ProductDTO product = productService.getProduct(productId);
+        return getProductDTOResponseEntity(product);
+    }
+
+    @ApiOperation("Update the Product")
+    @PutMapping(value = "/products/{productId}",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<ProductDTO> updateProduct(
+        @ApiParam("Title of the product")
+        @RequestParam("title") String title,
+        @ApiParam("description of the product")
+        @RequestParam("description") String description,
+        @ApiParam("price of the product")
+        @RequestParam("price") double price,
+        @ApiParam("discountedPrice of the product")
+        @RequestParam("discountedPrice") double discountedPrice,
+        @ApiParam("currency of price of the product")
+        @RequestParam("currency") String currency,
+        @ApiParam("smallImageUrl of the product")
+        @RequestParam("smallImageUrl") String smallImageUrl,
+        @ApiParam("mediumImageUrl of the product")
+        @RequestParam("mediumImageUrl") String mediumImageUrl,
+        @ApiParam("largeImageUrl of the product")
+        @RequestParam("largeImageUrl") String largeImageUrl,
+        @ApiParam("categoryId of the product")
+        @RequestParam("categoryId") String categoryId,
+        @ApiParam("storeId of the product")
+        @RequestParam("storeId") String storeId,
+        @ApiParam("brand of the product")
+        @RequestParam("brand") String brand,
+        @ApiParam("url of the product")
+        @RequestParam("url") String url,
+        @ApiParam("id of the product")
+        String productId
+    ) {
+        ProductDTO product = productService.updateProduct(productId, title, description, price, discountedPrice,
+            currency, smallImageUrl,
+            mediumImageUrl, largeImageUrl, categoryId, storeId, brand, url);
+        return getProductDTOResponseEntity(product);
+    }
+
+
+
+    private ResponseEntity<ProductDTO> getProductDTOResponseEntity(ProductDTO product) {
         Link selfLink = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(ProductResource.class)
             .getProduct(product.getProductId())).withSelfRel();
         product.add(selfLink);
--- product-and-offers-service-module/product-offer-service/src/main/java/com/kgcorner/topspin/service/ProductService.java
+++ product-and-offers-service-module/product-offer-service/src/main/java/com/kgcorner/topspin/service/ProductService.java
@@ -11,6 +11,7 @@ import com.kgcorner.topspin.dtos.factory.StoreFactory;
 import com.kgcorner.topspin.model.CategoryResponse;
 import com.kgcorner.topspin.model.StoreResponse;
 import com.kgcorner.topspin.persistence.ProductPersistenceLayer;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -71,5 +72,38 @@ public class ProductService {
             largeImageUrl, category, store, brand, String.format(storeDTO.getSurferPlaceHolder(), url));
         product = productPersistenceLayer.createProduct(product);
         return new ProductDTO(product);
+    }
+
+    public ProductDTO updateProduct(ProductDTO productDTO) {
+        Product product = productPersistenceLayer.getProduct(productDTO.getProductId());
+        BeanUtils.copyProperties(productDTO, product);
+        return new ProductDTO(product);
+    }
+
+    public ProductDTO updateProduct(String productId, String title, String description, double price,
+                                    double discountedPrice, String currency, String smallImageUrl,
+                                    String mediumImageUrl, String largeImageUrl, String categoryId,
+                                    String storeId, String brand, String url) {
+        Product existingProduct = productPersistenceLayer.getProduct(productId);
+        CategoryResponse categoryDTO;
+        StoreResponse storeDTO;
+        try {
+            categoryDTO = categoryClient.getCategory(categoryId);
+        } catch (Exception x) {
+            throw new IllegalArgumentException("Failed to find category with id " + categoryId, x);
+        }
+        try {
+            storeDTO = storeClient.get(storeId);
+        } catch (Exception x) {
+            throw new IllegalArgumentException("Failed to find store with id " + storeId, x);
+        }
+        var category = categoryFactory.createCategory(categoryDTO.getCategoryId(),
+            categoryDTO.getName(), categoryDTO.getDescription());
+        var store = storeFactory.createStore(storeDTO.getStoreId(), storeDTO.getName(), storeDTO.getDescription());
+        Product updatedProduct = productFactory.createProduct(title, description, price, discountedPrice,
+            currency, smallImageUrl, mediumImageUrl,
+            largeImageUrl, category, store, brand, String.format(storeDTO.getSurferPlaceHolder(), url));
+        BeanUtils.copyProperties(updatedProduct, existingProduct, "productId");
+        return new ProductDTO(productPersistenceLayer.updateProduct(existingProduct));
     }
 }

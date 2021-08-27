f531a2f46cd6df9439fd15d659ff67f16699f984
Kumar <kumargauravgupta3@gmail.com>
2021-08-27 15:23:48 +0530

    Adds feign client dependency in user service

--- packaging/executable-jar-packaging/spring-user-service/src/main/java/com/kgcorner/topspin/UserServiceApplication.java
+++ packaging/executable-jar-packaging/spring-user-service/src/main/java/com/kgcorner/topspin/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.kgcorner.topspin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {

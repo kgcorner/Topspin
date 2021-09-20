65e0ad5d3528f9375263afefbe728130cc972903
Kumar <kumargauravgupta3@gmail.com>
2021-09-20 22:17:05 +0530

    Add CORS to gateway

---
+++ packaging/executable-jar-packaging/spring-topspin-gateway/src/main/java/com/kgcorner/topspin/GatewayCorsConfiguration.java
@@ -0,0 +1,35 @@
+package com.kgcorner.topspin;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Description : <Write class Description>
+ * Author: kumar
+ * Created on : 05/09/21
+ */
+
+@Configuration
+public class GatewayCorsConfiguration {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH","DELETE", "PUT"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}

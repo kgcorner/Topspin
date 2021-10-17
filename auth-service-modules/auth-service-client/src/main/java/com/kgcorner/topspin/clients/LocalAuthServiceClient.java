4e951e89d055180ba742a33538303a565b517c4b
Kumar <kumargauravgupta3@gmail.com>
2021-10-17 13:33:42 +0530

    Adds more test

--- auth-service-modules/auth-service-client/src/main/java/com/kgcorner/topspin/clients/LocalAuthServiceClient.java
+++ auth-service-modules/auth-service-client/src/main/java/com/kgcorner/topspin/clients/LocalAuthServiceClient.java
@@ -5,6 +5,7 @@ import com.kgcorner.topspin.clients.model.LoginResponse;
 import com.kgcorner.topspin.clients.model.TokenResponse;
 import com.kgcorner.topspin.model.Token;
 import com.kgcorner.topspin.service.Authenticator;
+import com.kgcorner.topspin.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,28 +20,39 @@ public class LocalAuthServiceClient implements AuthServiceClient {
     @Autowired
     private Authenticator authenticator;
 
+    @Autowired
+    private RegistrationService registrationService;
+
     @Override
     public TokenResponse getToken(String token) {
-        TokenResponse tokenResponse = new TokenResponse();
         Token authenticationToken = authenticator.authenticateWithToken(token);
+        return getTokenResponse(authenticationToken);
+    }
+    @Override
+    public TokenResponse getTokenForOAuth(String token, String serverName) {
+        Token accessToken = authenticator.validateAccessTokenAndAuthorize(token, serverName);
+        return getTokenResponse(accessToken);
+    }
+
+    @Override
+    public TokenResponse resolveAccessToken(String token, String redirectUri, String serverName) {
+        Token token1 = authenticator.resolveTokenAndAuthorize(token, redirectUri, serverName);
+        return getTokenResponse(token1);
+    }
+
+    @Override
+    public LoginResponse createLogin(String userName, String password, String userId) {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    private TokenResponse getTokenResponse(Token authenticationToken) {
+        TokenResponse tokenResponse = new TokenResponse();
         tokenResponse.setAccessToken(authenticationToken.getAccessToken());
         tokenResponse.setRefreshToken(authenticationToken.getRefreshToken());
         tokenResponse.setExpiresInSeconds(authenticationToken.getExpiresInSeconds());
         return tokenResponse;
     }
 
-    @Override
-    public TokenResponse getTokenForOAuth(String token, String serverName) {
-        return null;
-    }
-
-    @Override
-    public TokenResponse resolveAccessToken(String token, String redirectUri, String serverName) {
-        return null;
-    }
-
-    @Override
-    public LoginResponse createLogin(String userName, String password, String userId) {
-        return null;
-    }
 }
